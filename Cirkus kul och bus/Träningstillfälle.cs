ee11ed12f483f6f0a24efe89f93922a292f71694
Daniel Edvinsson <edvinsson.d@gmail.com>
2016-05-24 13:49:40 +0200

    Skapat klasser

---
+++ Cirkus kul och bus/Träningstillfälle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cirkus_kul_och_bus
+{
+    class Träningstillfälle
+    {
+    }
+}
